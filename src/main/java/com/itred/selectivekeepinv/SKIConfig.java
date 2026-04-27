4055d0cbdfb8800f441c8369c158cec7ac820381
Itred12 <Redpup3@gmail.com>
2026-04-27 15:35:25 -0400

    Added config option to account for the KeepInvTag mod

--- src/main/java/com/itred/selectivekeepinv/SKIConfig.java
+++ src/main/java/com/itred/selectivekeepinv/SKIConfig.java
@@ -7,10 +7,14 @@ public class SKIConfig {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
     public static final ModConfigSpec.BooleanValue DISABLE_GRAVESTONES = BUILDER
-            .comment("Henkelmax's Gravestones: disable placing gravestones and/or granting obituaries for players with selectivekeepinventory enabled")
+            .comment("Henkelmax's Gravestones: \n\nDisable placing gravestones and/or granting obituaries for players with selectivekeepinventory enabled")
             .translation("selectivekeepinventory.config.disable_gravestones")
             .define("disableGravestones", false);
 
+    public static final ModConfigSpec.BooleanValue KEEPINVTAG_COMPAT = BUILDER
+            .comment("Widder's KeepInvTag: \n\nAutomatically enable SelectiveKeepInventory for players with the \"KeepInv\" command tag when they join, for if you were using the KeepInvTag mod beforehand.")
+            .translation("selectivekeepinventory.config.keepinvtag_compat")
+            .define("keepInvTagCompat", false);
 
     static final ModConfigSpec COMMON_CONFIG = BUILDER.build();
 
